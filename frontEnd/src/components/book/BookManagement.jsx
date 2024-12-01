b542e4ec56458f7fc4577185a46fca42a0393266
Sharon <sharonkunnel2003@gmail.com>
2024-12-01 15:21:55 +0530

    1.12.5

--- frontEnd/src/components/book/BookManagement.jsx
+++ frontEnd/src/components/book/BookManagement.jsx
@@ -1,8 +1,10 @@
+import { Link } from 'react-router-dom';
 import BookForm from './BookForm';
 import BookList from './BookList';
 const BookManagement = () => {
     return (
         <div className="container mx-auto px-4 py-8 max-w-xl">
+            <Link to='/auth/login'>click</Link>
           <h1 className="text-3xl font-bold text-center mb-6">Book Management App</h1>
           <BookForm />
           <div className="mt-6">
